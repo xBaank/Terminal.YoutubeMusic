2fb77e3351f84f77afac7dea274c9d5c039986bf
Roberto Blázquez <elroby444@gmail.com>
2024-07-21 13:31:07 +0200

    Add simple test

---
+++ Tests/PlayerTests.cs
@@ -0,0 +1,25 @@
+using Console.Audio;
+using FluentAssertions;
+using OpenTK.Audio.OpenAL;
+
+namespace Tests;
+
+public class PlayerTests
+{
+    [Fact]
+    public async Task I_can_play_a_song()
+    {
+        await using var player = new PlayerController() { Volume = 0 };
+
+        var video = (
+            await player.SearchAsync("https://www.youtube.com/watch?v=f8mL0_4GeV0")
+        ).First();
+
+        await player.SetAsync(video);
+        await player.PlayAsync();
+        await Task.Delay((player.TotalTime ?? default) + TimeSpan.FromSeconds(5));
+
+        player.State.Should().Be(ALSourceState.Stopped);
+        player.Song.Should().Be(video);
+    }
+}
