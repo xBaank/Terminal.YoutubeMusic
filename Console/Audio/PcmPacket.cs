1d4c9a1c8a21b5fc73cd83e1f599be19cdd4756c
Roberto Blázquez <elroby444@gmail.com>
2024-07-23 11:06:46 +0200

    Remove Array allocations  and use Array pool

---
+++ Console/Audio/PcmPacket.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Buffers;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console.Audio;
+
+internal struct PcmPacket(byte[] Data, int Lenght) : IDisposable
+{
+    private readonly byte[] _data = Data;
+    public readonly ReadOnlySpan<byte> Data => _data.AsSpan()[..Lenght];
+    public int Lenght { get; } = Lenght;
+
+    public readonly void Dispose() => ArrayPool<byte>.Shared.Return(_data);
+}
