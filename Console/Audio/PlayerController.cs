6cf555bf3fcff2bdf3ff52975ca3fe1424aebe8d
Roberto Blázquez <elroby444@gmail.com>
2024-07-23 16:34:49 +0200

    Fix CI (#5)
    * WIP
    
    * Update Readme.md
    
    * Add seek for keyboard only
    
    * Update Readme.md
    
    * Skip on error
    
    * Add other loop states
    
    * Update Readme.md
    
    * Update Readme.md
    
    * Update Readme.md
    
    * Add simple test
    
    * Fix test
    
    * Try to fix for env
    
    * WIP
    
    * More tests
    
    * Fix
    
    * Move files
    
    * Fix errors
    
    * Remove Array allocations  and use Array pool
    
    * Reduce loading time
    
    * WIP
    
    * Change skip order
    
    * Add error code
    
    * Use null device (default)
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * Try null
    
    * WIP
    
    * WIP
    
    * Increase sleep
    
    * WIP
    
    * WIP
    
    * WIP
    
    * maybe?
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * Update dotnet.yml
    
    * Update dotnet.yml
    
    * Update dotnet.yml
    
    * Update dotnet.yml
    
    * Update dotnet.yml
    
    * Update dotnet.yml
    
    * Update dotnet.yml
    
    * Update dotnet.yml

--- Console/Audio/PlayerController.cs
+++ Console/Audio/PlayerController.cs
@@ -55,7 +55,7 @@ public class PlayerController : IAsyncDisposable
 
     public PlayerController()
     {
-        _device = ALC.OpenDevice(null);
+        _device = ALC.OpenDevice(Environment.GetEnvironmentVariable("DeviceName"));
         _context = ALC.CreateContext(_device, new ALContextAttributes());
         ALC.MakeContextCurrent(_context);
 
