6e3cad7e3504835c7a01cf763423d45c1b6e9afa
Roberto Blázquez <elroby444@gmail.com>
2024-07-30 12:56:01 +0200

    WIP

---
+++ Console/Views/Loader.cs
@@ -0,0 +1,34 @@
+﻿using System.Runtime.CompilerServices;
+using Terminal.Gui;
+
+namespace Console.Views;
+
+internal abstract class Loader(View view)
+{
+    private SpinnerView? spinner = null;
+
+    public void ShowLoading()
+    {
+        view.RemoveAll();
+
+        spinner?.Dispose();
+        spinner = new SpinnerView
+        {
+            X = Pos.Center(),
+            Y = Pos.Center(),
+            Width = Dim.Auto(),
+            Height = Dim.Auto(),
+            Visible = true,
+            Style = new SpinnerStyle.BouncingBall(),
+            AutoSpin = true,
+        };
+
+        view.Add(spinner);
+    }
+
+    public void HideLoading()
+    {
+        spinner?.Dispose();
+        view.Remove(spinner);
+    }
+}
