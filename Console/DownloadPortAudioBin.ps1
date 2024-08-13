ef003d68f43ac4b11fda1be438b4b345ba06c50f
Roberto Blázquez <elroby444@gmail.com>
2024-08-13 20:44:42 +0200

    Add specific platforms

---
+++ Console/DownloadPortAudioBin.ps1
@@ -0,0 +1,42 @@
+param (
+    [string]$platform,
+    [string]$outputPath
+)
+
+$ErrorActionPreference = "Stop"
+
+# Normalize platform identifier
+$platform = $platform.ToLower().Replace("win-", "windows-")
+
+# Check if already exists
+if (Test-Path $outputPath) {
+    Write-Host "Skipped downloading Portaudio, file already exists."
+    exit
+}
+
+# Download the archive
+Write-Host "Downloading Portaudio..."
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+$http = New-Object System.Net.WebClient
+try {
+    $http.DownloadFile("https://github.com/xBaank/PortAudioBin/releases/download/19.7/portaudio-$platform.zip", "$outputPath.zip")
+} finally {
+    $http.Dispose()
+}
+
+try {
+    # Extract FFmpeg
+    Add-Type -Assembly System.IO.Compression.FileSystem
+    $zip = [IO.Compression.ZipFile]::OpenRead("$outputPath.zip")
+    try {
+        $fileName = If ($platform.Contains("windows-")) { "bin/portaudio.dll" } ElseIf ($platform.Contains("linux-")) { "lib/libportaudio.so" } Else { "lib/libportaudio.dylib" } 
+        [IO.Compression.ZipFileExtensions]::ExtractToFile($zip.GetEntry($fileName), $outputPath)
+    } finally {
+        $zip.Dispose()
+    }
+
+    Write-Host "Done downloading Portaudio."
+} finally {
+    # Clean up
+    Remove-Item "$outputPath.zip" -Force
+}
